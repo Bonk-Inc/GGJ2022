199f768c1031b609f607bc35b680c76805d20b11
Traxx186 <justinvanderkruit@icloud.com>
2022-01-29 18:11:01 +0100

    Merge branch 'feature/karma-system' of github.com:jvdwijk/GGJ2022 into development

---
+++ Assets/Scripts/Characters/Karma/Karma.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+
+public class Karma : MonoBehaviour
+{
+    [SerializeField] 
+    private int maxKarma;
+
+    public event EventHandler<KarmaChangeArgs> OnKarmaChange; 
+    
+    public KarmaState KarmaState { get; private set; }
+
+    private int currentKarma;
+
+    private void Awake()
+    {
+        currentKarma = maxKarma / 2;
+        KarmaState = KarmaState.Human;
+    }
+
+    private KarmaState DetermineKarmaState(float karmaPercentage) => karmaPercentage switch
+    {
+        <= 40f => KarmaState.Demon,
+        >= 60f => KarmaState.Angel,
+        _ => KarmaState.Human
+    };
+
+    public void Increase(int amount) => SetKarma(currentKarma + amount);
+
+    public void Decrease(int amount) => SetKarma(currentKarma - amount);
+    
+    private void SetKarma(int karma)
+    {
+        currentKarma = (karma >= maxKarma) ? maxKarma : (karma <= 0) ? 0 : karma;
+
+        var previousKarma = KarmaState;
+        var karmaPercentage = (100f / maxKarma) * currentKarma;
+        KarmaState = DetermineKarmaState(karmaPercentage);
+
+        if (KarmaState != previousKarma)
+            CallEvent(previousKarma);
+    }
+
+    private void CallEvent(KarmaState previousKarmaState)
+    {
+        var eventArgs = new KarmaChangeArgs
+        {
+            previousKarmaState = previousKarmaState,
+            newKarmaState = KarmaState
+        };
+        
+        OnKarmaChange?.Invoke(this, eventArgs);
+    }
+
+    public class KarmaChangeArgs
+    {
+        public KarmaState previousKarmaState, newKarmaState;
+    }
+}
