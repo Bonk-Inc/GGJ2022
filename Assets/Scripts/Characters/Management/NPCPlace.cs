b4f65175408d97ade347ba456ffe0cb96a236bdf
jvdwijk <joeyvdwijk@gmail.com>
2022-01-30 16:29:43 +0100

    map edits + sprites smaller

--- Assets/Scripts/Characters/Management/NPCPlace.cs
+++ Assets/Scripts/Characters/Management/NPCPlace.cs
@@ -59,6 +59,7 @@ public class NPCPlace : MonoBehaviour
         var npcs = new List<Transform>();
         foreach (var spot in npcSpots)
         {
+            if(transform == null) continue;
             if(transform.childCount != 0) npcs.Add(spot.transform.GetChild(0));
         }
         return npcs;
